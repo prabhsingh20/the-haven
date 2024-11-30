7ef8f1594e6e6dd402b4c05cfe72284d2d132f01
Prabh ‪‪ <singh20prabh@gmail.com>
2024-11-30 18:22:32 +0530

    first commit

---
+++ app/account/page.js
@@ -0,0 +1,13 @@
+export const metadata = { title: "Guest area" };
+
+function Page() {
+  return (
+    <div>
+      <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+        Welcome, Prabh
+      </h2>
+    </div>
+  );
+}
+
+export default Page;
---
+++ app/loading.js
@@ -0,0 +1,5 @@
+function Loading() {
+  return <div>Data Loading...</div>;
+}
+
+export default Loading;

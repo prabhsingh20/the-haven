6e797867c8bbbabfb0461091625dcfa897b0ae69
Prabh ‪‪ <singh20prabh@gmail.com>
2025-01-23 12:12:03 +0530

    dateSelector

--- app/_components/DeleteReservation.js
+++ app/_components/DeleteReservation.js
@@ -3,14 +3,13 @@
 import { TrashIcon } from "@heroicons/react/24/solid";
 import { useTransition } from "react";
 import SpinnerMini from "./SpinnerMini";
-import { deleteBooking } from "../_lib/actions";
 
 function DeleteReservation({ bookingId, onDelete }) {
   const [isPending, startTransition] = useTransition();
 
   function handleDelete() {
     if (confirm("Are you sure you want to delete this reservation?"))
-      startTransition(() => deleteBooking(bookingId));
+      startTransition(() => onDelete(bookingId));
   }
 
   return (
